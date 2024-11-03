1af51126b18c965912da811336ac12caa573cead
salehshakib <salehshakib.ss@gmail.com>
2024-11-03 19:06:06 +0600

    update

--- asset/js/profile-swiper.js
+++ asset/js/profile-swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".custom-swiper", {
   // Optional parameters
   loop: true,
   autoplay: {
@@ -6,21 +6,21 @@ const swiper = new Swiper(".swiper", {
     disableOnInteraction: false, // Allows autoplay to continue after user interaction
   },
 
-  // If we need pagination
+  // Pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: ".custom-swiper-pagination",
     clickable: true, // Makes pagination bullets clickable
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".custom-swiper-button-next",
+    prevEl: ".custom-swiper-button-prev",
   },
 
-  // And if we need scrollbar
+  // Scrollbar
   scrollbar: {
-    el: ".swiper-scrollbar",
+    el: ".custom-swiper-scrollbar",
     hide: true, // Optional: hides the scrollbar when not in use
   },
 });
