bcc1a580ad74cb0b1bbe87bf721653e3722c1a89
Saleh Shakib <salehshakib.ss@gmail.com>
2024-11-06 17:40:19 +0600

    update for influencer dashboard

--- asset/js/add-shortcode.js
+++ asset/js/add-shortcode.js
@@ -7,15 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
     newShortcode.className = "flex space-x-2 mt-2";
     newShortcode.innerHTML = `
   <input
-    type="text"
-    placeholder="Object name"
-    class="block w-full p-2 py-1 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-sm "
-  />
-  <input
-    type="text"
-    placeholder="Key"
-    class="block w-full p-2 py-1 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-sm "
-  />
+                      type="text"
+                      name="key[]"
+                      placeholder="Key"
+                      class="border border-gray-300 text-gray-900 rounded-lg block w-full pe-10 p-2.5"
+                    />
   <button type="button" class="remove-shortcode text-red-500 font-bold rounded-full">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
